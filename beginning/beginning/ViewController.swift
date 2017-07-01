49fb83d09a64a1145f25c3c82c5e3e7b0db4050b
Hiltonsmith <hiltonsmith@Hiltonsmiths-MacBook-Pro.local>
2017-07-01 10:48:56 -0400

    added button

--- beginning/beginning/ViewController.swift
+++ beginning/beginning/ViewController.swift
@@ -8,16 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
+    var count = 0
+    let messeges = ["Hello World", "how are you", "How is the weather","It is Sunny"]
+    @IBOutlet weak var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        label.textColor = UIColor.red
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        count = count + 1
+        label.text = messeges[count % messeges.count]
     }
 
 
